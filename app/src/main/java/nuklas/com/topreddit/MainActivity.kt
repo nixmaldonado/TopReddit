6e23a1ed4d172e51800b62ae0085f54315290734
Nicolas <nixmaldonado@gmail.com>
2018-10-27 17:31:42 -0300

    Initializes presenter in MainActivity.

--- app/src/main/java/nuklas/com/topreddit/MainActivity.kt
+++ app/src/main/java/nuklas/com/topreddit/MainActivity.kt
@@ -1,12 +1,15 @@
 package nuklas.com.topreddit
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val presenter: MainActivityPresenter = MainActivityPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        presenter.initialize()
     }
 }
