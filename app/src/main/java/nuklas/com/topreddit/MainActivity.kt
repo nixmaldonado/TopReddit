dd81cca2a56c5d87d5ca0eb0c2d557079962201b
Nicolas <nixmaldonado@gmail.com>
2018-10-27 17:53:37 -0300

    Creates fragments in MainActivity and sets PostListFragment as first one.

--- app/src/main/java/nuklas/com/topreddit/MainActivity.kt
+++ app/src/main/java/nuklas/com/topreddit/MainActivity.kt
@@ -6,10 +6,15 @@ import android.support.v7.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     private val presenter: MainActivityPresenter = MainActivityPresenter(this)
+    val postFragment: PostFragment = PostFragment()
+    val postListFragment: PostListFragment = PostListFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, postListFragment).commit()
         presenter.initialize()
     }
 }
