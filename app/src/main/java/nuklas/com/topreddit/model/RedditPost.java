6206ff8582e292727ea1b89716cdf9ad22125195
Nicolas <nixmaldonado@gmail.com>
2018-10-28 14:56:15 -0300

    Implements Parcelable in RedditPost data class.

--- app/src/main/java/nuklas/com/topreddit/model/RedditPost.java
+++ app/src/main/java/nuklas/com/topreddit/model/RedditPost.java
@@ -1,5 +1,7 @@
 package nuklas.com.topreddit.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import com.google.gson.annotations.SerializedName;
 
 public class RedditPost {
@@ -7,7 +9,7 @@ public class RedditPost {
     @SerializedName("data")
     public PostData data;
 
-    public class PostData {
+    public static class PostData implements Parcelable {
 
         @SerializedName("author")
         public String author;
@@ -15,7 +17,7 @@ public class RedditPost {
         @SerializedName("title")
         public String title;
 
-        @SerializedName("created")
+        @SerializedName("created_utc")
         public long created;
 
         @SerializedName("num_comments")
@@ -23,5 +25,39 @@ public class RedditPost {
 
         @SerializedName("thumbnail")
         public String imageThumbnailUrl;
+
+        protected PostData(Parcel in) {
+            author = in.readString();
+            title = in.readString();
+            created = in.readLong();
+            commentCount = in.readInt();
+            imageThumbnailUrl = in.readString();
+        }
+
+        public static final Creator<PostData> CREATOR = new Creator<PostData>() {
+            @Override
+            public PostData createFromParcel(Parcel in) {
+                return new PostData(in);
+            }
+
+            @Override
+            public PostData[] newArray(int size) {
+                return new PostData[size];
+            }
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeString(author);
+            dest.writeString(title);
+            dest.writeLong(created);
+            dest.writeInt(commentCount);
+            dest.writeString(imageThumbnailUrl);
+        }
     }
 }
